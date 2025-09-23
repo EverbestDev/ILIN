d4b316011639782bfadea030a3f23dde39e1f4ff
Usamah Olawoore <olawooreusamahabidemi@example.com>
2025-09-23 12:10:51 +0100

    testing commit

--- ili-nigeria-frontend/src/pages/ContactPage.jsx
+++ ili-nigeria-frontend/src/pages/ContactPage.jsx
@@ -158,7 +158,7 @@ export default function ContactPage() {
 
     setLoading(true);
     try {
-      const response = await fetch("http://localhost:5000/api/contact", {
+      const response = await fetch("https://ilin-backend.onrender.com/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
