63345fa997af19fc502153506932200dc6683183
Usamah Olawoore <olawooreusamahabidemi@example.com>
2025-10-04 23:40:41 +0100

    backend ii

--- ili-nigeria-frontend/src/pages/Admin/Dashboard.jsx
+++ ili-nigeria-frontend/src/pages/Admin/Dashboard.jsx
@@ -16,7 +16,7 @@ import {
 } from "lucide-react";
 
 const Dashboard = () => {
-  const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+  const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
   const [loading, setLoading] = useState(true);
 
   const [stats, setStats] = useState({
