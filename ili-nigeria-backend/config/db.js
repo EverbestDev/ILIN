5d6bb1a8e41e731ab3b3b5f32f654a62d8fee8c0
GIT-OLAWOORE <olawooreusamahabidemi@example.com>
2025-10-05 13:49:08 +0100

    backend ii

--- ili-nigeria-backend/config/db.js
+++ ili-nigeria-backend/config/db.js
@@ -20,7 +20,7 @@ import { startTaskReminder } from "../utils/taskReminder.js";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
