f88db0d97c53e14d63ac9e3cf21263c0c2279ef2
GIT-OLAWOORE <olawooreusamahabidemi@example.com>
2025-10-05 13:58:52 +0100

    backend ii

--- ili-nigeria-backend/config/db.js
+++ ili-nigeria-backend/config/db.js
@@ -1,31 +1,10 @@
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-//   try {
-//     // 👀 Make sure we are reading from .env
-//     const conn = await mongoose.connect(process.env.MONGO_URI);
-
-//     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.error("❌ MongoDB connection error:", error.message);
-//     process.exit(1); // Stop app if DB fails
-//   }
-// };
-
-// export default connectDB;
-
 // src/config/db.js
 import mongoose from "mongoose";
-import { startTaskReminder } from "../utils/taskReminder.js";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB");
-    startTaskReminder(); // Start cron job for task reminders
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error);
     process.exit(1);
